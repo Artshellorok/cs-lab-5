c51d8045dc20bb35e99681d9ed3b2d54fde23ac2
artshellorok <artshellorok@gmail.com>
2022-12-09 22:38:17 +0300

    mario

--- mario/run.bash
+++ mario/run.bash
@@ -3,14 +3,16 @@
 xhost +local:docker || true
 
 docker run  -ti --rm \
+            --user "$(id -u)" \
+            -v "/run/user/$UID/pulse/native:/var/run/pulseaudio.sock" \
             -e "DISPLAY" \
             -e "QT_X11_NO_MITSHM=1" \
             -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
             --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
             -e XAUTHORITY \
-            -v "/home:/root" \
             -v "/dev:/dev" \
+            --device /dev/snd \
             --net=host \
             --privileged \
-            --name mymario_image mymario_image \
+            --name mario mario \
             bash
